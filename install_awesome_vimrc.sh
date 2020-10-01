9dd31b2e92bccc308b63de1dd789c3af54b75e47
johnsonsiumen <31721748+yu-chun-kit@users.noreply.github.com>
2020-10-01 23:17:37 +0800

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 set -e
 
-cd /cygdrive/c/Users/zeng/.vim_runtime
-#cd ~/.vim_runtime
+# cd /cygdrive/c/Users/zeng/.vim_runtime
+cd ~/.vim_runtime
 
-echo 'set runtimepath+=/cygdrive/c/Users/zeng/.vim_runtime
-#echo 'set runtimepath+=~/.vim_runtime
+# echo 'set runtimepath+=/cygdrive/c/Users/zeng/.vim_runtime
+echo 'set runtimepath+=~/.vim_runtime
 
-source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/basic.vim
-source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/filetypes.vim
-source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/plugins_config.vim
-source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/extended.vim
-#source ~/.vim_runtime/vimrcs/basic.vim
-#source ~/.vim_runtime/vimrcs/filetypes.vim
-#source ~/.vim_runtime/vimrcs/plugins_config.vim
-#source ~/.vim_runtime/vimrcs/extended.vim
+# source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/basic.vim
+# source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/filetypes.vim
+# source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/plugins_config.vim
+# source /cygdrive/c/Users/zeng/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
 
 try
-source /cygdrive/c/Users/zeng/.vim_runtime/my_configs.vim
-#source ~/.vim_runtime/my_configs.vim
+# source /cygdrive/c/Users/zeng/.vim_runtime/my_configs.vim
+source ~/.vim_runtime/my_configs.vim
 catch
-endtry' > /cygdrive/c/Users/zeng/.vimrc
-#endtry' > ~/.vimrc
+endtry' > ~/.vimrc
+# endtry' > /cygdrive/c/Users/zeng/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
